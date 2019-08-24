89053308e28de76ad42b4806db1b8ee56cb54208
Dan Smith <daniel.smith8795@gmail.com>
2019-08-23 23:08:43 -0500

    added if statement to prevent ordering more than what is available

--- customer.js
+++ customer.js
@@ -11,63 +11,68 @@ var connection = mysql.createConnection({
 });
 var available_items = [];
 
-
-connection.connect(function (err) {
-    if (err) throw err;
-    connection.query("SELECT*FROM products;", function (err, res) {
+var listItems = function () {
+    connection.connect(function (err) {
         if (err) throw err;
-        for (var i = 0; i < res.length; i++) {
-            console.log("Item ID: " + res[i].item_id);
-            console.log("Product Name: " + res[i].product_name);
-            console.log("Department: " + res[i].department_name);
-            console.log("Price: " + res[i].price);
-            console.log("Amount in stock: " + res[i].stock_quantity);
-            console.log("==========================================");
-            available_items.push(res[i]);
-        }
-    });
-    connection.end()
-})
-var checkout = function () {
-    inquirer
-        .prompt([
-            {
-                name: "chosenID",
-                message: "Please choose the ID of the item you would like to buy",
-                type: "choice",
-                choices: [available_items]
-            },
-            {
-                name: "quantity",
-                type: "number",
-                message: "How many would you like to buy?",
-                type: "input"
-            },
-            {
-                type: "confirm",
-                message: "Confirm order?",
-                default: true,
-                name: "ready"
+        connection.query("SELECT*FROM products;", function (err, res) {
+            if (err) throw err;
+            for (var i = 0; i < res.length; i++) {
+                console.log("Item ID: " + res[i].item_id);
+                console.log("Product Name: " + res[i].product_name);
+                console.log("Department: " + res[i].department_name);
+                console.log("Price: " + res[i].price);
+                console.log("Amount in stock: " + res[i].stock_quantity);
+                console.log("==========================================");
+                available_items.push(res[i]);
             }
-        ])
-        .then(function (response) {
-            if (response.ready) {
-                console.log(response);
-                connection.connect(function (err) {
-                    if (err) throw err;
-                    connection.query("SELECT*FROM products WHERE item_id=" + response.chosenID + ";", function (err, res) {
-                        if (err) throw err;
-                        console.log("Item ID: " + res[i].item_id);
-                        console.log("Product Name: " + res[i].product_name);
-                        console.log("Department: " + res[i].department_name);
-                        console.log("Price: " + res[i].price);
-                        console.log("Amount in stock: " + res[i].stock_quantity);
-                        console.log("==========================================");
-                    });
-                    connection.end();
-                })
-
-            }
-        })
+        });
+        connection.end()
+    })
 }
+
+var checkout = function () {
+    inquirer.prompt([
+        {
+            name: "chosenID",
+            message: "Please choose the ID of the item you would like to buy",
+            type: "choice",
+            choices: [available_items]
+        },
+        {
+            name: "quantity",
+            type: "number",
+            message: "How many would you like to buy?",
+            type: "input"
+        },
+        {
+            type: "confirm",
+            message: "Confirm order?",
+            default: true,
+            name: "ready"
+        }
+    ]).then(function (response) {
+        if (response.ready) {
+            var shoppingCart = available_items[parseInt(response.chosenID) - 1];
+            var stockLeft = parseInt(shoppingCart.stock_quantity);
+            var requestedAmount = parseInt(response.quantity);
+            console.log(response);
+            if (requestedAmount <= stockLeft) {
+                console.log("Item ID: " + shoppingCart.item_id);
+                console.log("Product Name: " + shoppingCart.product_name);
+                console.log("Department: " + shoppingCart.department_name);
+                console.log("Price: " + shoppingCart.price);
+                console.log("New stock quantity: " + shoppingCart.stock_quantity);
+                console.log("==========================================");
+            }
+            else {
+                console.log("There is insufficient stock. Please choose a different amount.");
+                listItems()
+                checkout()
+            }
+        }
+
+    })
+}
+
+listItems();
 checkout();
