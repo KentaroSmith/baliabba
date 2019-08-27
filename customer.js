e745230d1b13d8db1c3e5de17f7eefbb2b1844f6
Dan Smith <daniel.smith8795@gmail.com>
2019-08-26 20:54:35 -0500

    made update to purchase functions

--- customer.js
+++ customer.js
@@ -1,8 +1,11 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+let mysql = require("mysql");
+let inquirer = require("inquirer");
 require("dotenv").config();
-
-var connection = mysql.createConnection({
+//first list items
+// prompt for selection and quantity then confirm
+//if yes then update new quantity
+//ask if they want to buy more
+let connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -11,68 +14,102 @@ var connection = mysql.createConnection({
 });
 var available_items = [];
 
-var listItems = function () {
-    connection.connect(function (err) {
+function listItems() {
+    connection.query("SELECT*FROM products;", function (err, res) {
         if (err) throw err;
-        connection.query("SELECT*FROM products;", function (err, res) {
-            if (err) throw err;
-            for (var i = 0; i < res.length; i++) {
-                console.log("Item ID: " + res[i].item_id);
-                console.log("Product Name: " + res[i].product_name);
-                console.log("Department: " + res[i].department_name);
-                console.log("Price: " + res[i].price);
-                console.log("Amount in stock: " + res[i].stock_quantity);
-                console.log("==========================================");
-                available_items.push(res[i]);
-            }
-        });
-        connection.end()
-    })
+        for (var i = 0; i < res.length; i++) {
+            console.log("Item ID: " + res[i].item_id);
+            console.log("Product Name: " + res[i].product_name);
+            console.log("Department: " + res[i].department_name);
+            console.log("Price: " + res[i].price);
+            console.log("Amount in stock: " + res[i].stock_quantity);
+            console.log("==========================================");
+            available_items.push(res[i]);
+        }
+    });
+    console.log("\n")
+    checkout();
 }
+connection.connect(function (err) {
+    if (err) throw err;
+    listItems();
+    //connection.end()
+})
+
 
 var checkout = function () {
     inquirer.prompt([
         {
-            name: "chosenID",
-            message: "Please choose the ID of the item you would like to buy",
-            type: "choice",
-            choices: [available_items]
-        },
-        {
-            name: "quantity",
-            type: "number",
-            message: "How many would you like to buy?",
-            type: "input"
-        },
-        {
-            type: "confirm",
-            message: "Confirm order?",
-            default: true,
-            name: "ready"
+            name: "choice",
+            message: "would you like to buy something?",
+            type: "confirm"
         }
     ]).then(function (response) {
-        if (response.ready) {
-            var shoppingCart = available_items[parseInt(response.chosenID) - 1];
-            var stockLeft = parseInt(shoppingCart.stock_quantity);
-            var requestedAmount = parseInt(response.quantity);
-            console.log(response);
-            if (requestedAmount <= stockLeft) {
-                console.log("Item ID: " + shoppingCart.item_id);
-                console.log("Product Name: " + shoppingCart.product_name);
-                console.log("Department: " + shoppingCart.department_name);
-                console.log("Price: " + shoppingCart.price);
-                console.log("New stock quantity: " + shoppingCart.stock_quantity);
-                console.log("==========================================");
-            }
-            else {
-                console.log("There is insufficient stock. Please choose a different amount.");
-                listItems()
-                checkout()
-            }
+        if (response.choice) {
+            inquirer.prompt([
+                {
+                    name: "chosenID",
+                    message: "Please choose the ID of the item you would like to buy",
+                    type: "number",
+                    type: "input"
+                },
+                {
+                    name: "quantity",
+                    type: "number",
+                    message: "How many would you like to buy?",
+                    type: "input"
+                },
+                {
+                    type: "confirm",
+                    message: "Confirm order?",
+                    default: true,
+                    name: "ready"
+                }
+            ]).then(function (response) {
+                if (response.ready) {
+                    var shoppingCart = available_items[parseInt(response.chosenID) - 1];
+                    var stockLeft = parseInt(shoppingCart.stock_quantity);
+                    var requestedAmount = parseInt(response.quantity);
+                    console.log(response);
+                    if (requestedAmount <= stockLeft) {
+                        console.log("Item ID: " + shoppingCart.item_id);
+                        console.log("Product Name: " + shoppingCart.product_name);
+                        console.log("Department: " + shoppingCart.department_name);
+                        console.log("Price: " + shoppingCart.price);
+                        var newAmount = parseInt(stockLeft - requestedAmount)
+                        console.log("New stock quantity: " + newAmount);
+                        console.log("==========================================");
+                        connection.query(
+                            "UPDATE products SET stock_quantity = ? WHERE item_id = ?;"[newAmount, shoppingCart.item_id],
+                            console.log(newAmount, shoppingCart.item_id),
+                            function (error) {
+                                if (error) throw (error);
+
+
+                            }
+                        );
+                        listItems();
+                    }
+                    else {
+                        console.log("There is insufficient stock. Please choose a different amount.");
+                        listItems();
+                    }
+                }
+                else {
+                    listItems();
+
+                }
+
+            });
         }
+        else {
+            connection.end();
+        }
+    }
 
-    })
-}
+    )
+};
 
-listItems();
-checkout();
+
+
+
